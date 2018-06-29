fd3c44b0fe9f4b083233d3931e76d76b71f021eb
Rylan <rylans@gmail.com>
2018-06-29 11:48:47 -0700

    Basic next rhyme implementation

---
+++ index.js
@@ -0,0 +1,36 @@
+(function() {
+  datamuse = require('datamuse');
+  
+  module.exports.of = (context, callback) => {
+    const prevLine = context.prev;
+    const lastWordPrevLine = prevLine.split(' ').pop();
+
+    const thisLine = context.current;
+    const lastWordThisLine = thisLine.split(' ').pop();
+
+    next(lastWordThisLine, (nextJson) => {
+      rhyme(lastWordPrevLine, (rhymeJson) => {
+	let nextRhymes = [];
+
+	for (i = 0; i < nextJson.length; i++) {
+	  for (j = 0; j < rhymeJson.length; j++) {
+	    if (rhymeJson[j].word === nextJson[i].word) {
+	      nextRhymes.push(nextJson[i].word);
+	    }
+	  }
+	}
+
+	callback(nextRhymes);
+      });
+    });
+  }
+
+  function next(q, f) {
+    datamuse.request('words?lc='+q).then(f);
+  }
+
+  function rhyme(q, f) {
+    datamuse.request('words?rel_rhy=' + q).then(f);
+  }
+
+})();
